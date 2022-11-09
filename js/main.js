c19673250c7bfac0457cc047e56e3c865bcdf6d1
Erik Tomlinson <c2024777@hallam.shu.ac.uk>
2022-11-09 10:59:37 +0000

    a

--- js/main.js
+++ js/main.js
@@ -12,7 +12,32 @@ let eventBtn=document.getElementById("myTestEvent")
 eventBtn.addEventListener("click",myEvent)
 function myEvent(ev){
     console.dir(ev)
+    ev.target.innerHTML = "Changed"
     console.info("I was clicked")
 }
+let colourBtns = document.querySelectorAll(".colPicker")
+console.dir(colourBtns) 
+for(let i=0; i<colourBtns.length; i++)
+{
+    colourBtns[i].addEventListener("click",chgColour)
+}
 
+function chgColour(ev){
+    console.info(ev.target.classList[0])
+    let newBgColour = ev.target.classList[0]+"Back"
+    document.querySelector("body").setAttribute("class",newBgColour)
+}
+//background color selector
+/*document.querySelector(".red").addEventListener("click",function(){
+    document.querySelector("body").setAttribute("class","redBack")
+})
+document.querySelector(".green").addEventListener("click",function(){
+    document.querySelector("body").setAttribute("class","greenBack")
+})
+document.querySelector(".blue").addEventListener("click",function(){
+    document.querySelector("body").setAttribute("class","blueBack")
+})
+document.querySelector(".reset").addEventListener("click",function(){
+    document.querySelector("body").removeAttribute("class")
+})*/
 })()
